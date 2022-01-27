992736217632eb6e2ef6ce422b30e15279e8fb72
Ereeh <80178277+Ereeh@users.noreply.github.com>
2022-01-27 14:19:47 +0100

    home
    aa

--- MinProject/src/Controller/HomeController.php
+++ MinProject/src/Controller/HomeController.php
@@ -6,15 +6,28 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AuteurRepository;
+use App\Repository\CategorieRepository;
+use App\Repository\LivreRepository;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request ,AuteurRepository $auteurRepository, CategorieRepository $categorieRepository,LivreRepository $livreRepository): Response
     {
+        
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            
+            'auteurs' => $auteurRepository->findAll(),     
+            'categories' => $categorieRepository->findAll(),      
+            'livres' => $livreRepository->findAll(),      
         ]);
+      
     }
 
 
