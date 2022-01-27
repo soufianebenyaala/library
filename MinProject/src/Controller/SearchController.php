856f8d92e0eba3eb8e19dc18c3c51660b1634258
Ereeh <80178277+Ereeh@users.noreply.github.com>
2022-01-27 13:22:45 +0100

    filtre
    filtre categorie

--- MinProject/src/Controller/SearchController.php
+++ MinProject/src/Controller/SearchController.php
@@ -6,9 +6,11 @@ use App\Repository\AuteurRepository;
 use App\Repository\CategorieRepository;
 use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class SearchController extends AbstractController
 {
@@ -18,18 +20,34 @@ class SearchController extends AbstractController
         //On recupere les filtres
         $filters = $request->get("categories");
         
+        $livres = $livreRepository->getlivrefilters($filters);
+      
+        
+        
         //On verifie si on a une requete Ajax 
         if($request->get('ajax')){
-            return "Ok";
+            
+            return new JsonResponse(
+                [
+                    'content' => $this->renderView('search/_content.html.twig', [
+                        'controller_name' => 'SearchController',
+                        'auteurs' => $auteurRepository->findAll(),           
+                        'livres' => $livres,    
+                    ])
+                ]
+            );
         }
         
-        $livres = $livreRepository->getlivrefilters($filters);
+    
+
+     
+    
 
         return $this->render('search/index.html.twig', [
             'controller_name' => 'SearchController',
             'auteurs' => $auteurRepository->findAll(),      
             'categories' => $categorieRepository->findAll(),      
-            'livres' => $livreRepository->findAll(),      
+            'livres' => $livres,      
         ]);
     }
 }
